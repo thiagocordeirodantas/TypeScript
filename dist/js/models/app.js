4ee0d2bad5096795338023e3e246bccccb667942
Thiago Cordeiro Dantas <thiiaguinhocordeirodantas@gmail.com>
2023-04-13 19:37:52 -0300

    modelando as regras

--- dist/js/models/app.js
+++ dist/js/models/app.js
@@ -1,1 +1,10 @@
-alert("Oii")
+import { Negociacao } from "./negociacao.js";
+
+
+const negociacao = new Negociacao(new Date(), 10,300);
+
+console.log(negociacao);
+
+negociacao.quantidade = 3000;
+
+console.log(negociacao)
