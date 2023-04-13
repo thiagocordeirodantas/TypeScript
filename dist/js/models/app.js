b5825ddd75126f7bae5973775b206ca916a95be4
Thiago Cordeiro Dantas <thiiaguinhocordeirodantas@gmail.com>
2023-04-13 20:06:01 -0300

    finalizando modulo

--- dist/js/models/app.js
+++ dist/js/models/app.js
@@ -3,8 +3,4 @@ import { Negociacao } from "./negociacao.js";
 
 const negociacao = new Negociacao(new Date(), 10,300);
 
-console.log(negociacao);
-
-negociacao.quantidade = 3000;
-
-console.log(negociacao)
+console.log(negociacao.volume);
